98e600bbb3c36aa985aa483f0e75a87aa3941d08
Min Hu <lebonthe@gmail.com>
2024-01-30 20:36:21 +0800

    change error messages to enum

---
+++ log-in and sign-up/AlertText.swift
@@ -0,0 +1,19 @@
+//
+//  Content.swift
+//  log-in and sign-up
+//
+//  Created by Min Hu on 2024/1/29.
+//
+
+import Foundation
+
+//struct Content {
+//    var logIn: Bool = true
+//    let alertText: [AlertText] = []
+//}
+
+struct AlertText {
+    let title: String
+    let message: String    
+}
+
